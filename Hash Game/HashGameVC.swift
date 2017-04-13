88b8871ca3071802bd6728cfce54e02bac7e0b2b
Kesley Ribeiro <kesley.developer@icloud.com>
2017-04-13 17:48:49 -0300

    Tabuleiro e peças do jogo agora estão randômicos, novo jogo, novas peças e layout.

--- Hash Game/HashGameVC.swift
+++ Hash Game/HashGameVC.swift
@@ -33,17 +33,28 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
     // Combinations possible to winner game
     let correctCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     
-    //list of Images in array
+    // List of images in array 
     let arrayOfImages : NSArray = [UIImage(named: "2 star.png")!,
-                                   UIImage(named: "Arrows.png")!,
-                                   UIImage(named: "Battery.png")!,
-                                   UIImage(named: "Heart.png")!,
-                                   UIImage(named: "Oval.png")!,
-                                   UIImage(named: "Polygon.png")!,
-                                   UIImage(named: "Rectangle edited.png")!,
-                                   UIImage(named: "Star.png")!,
-                                   UIImage(named: "Triangle.png")!,
-                                   UIImage(named: "X.png")!]
+                                 UIImage(named: "Arrows.png")!,
+                                 UIImage(named: "Battery.png")!,
+                                 UIImage(named: "Heart.png")!,
+                                 UIImage(named: "Oval.png")!,
+                                 UIImage(named: "Polygon.png")!,
+                                 UIImage(named: "Rectangle edited.png")!,
+                                 UIImage(named: "Star.png")!,
+                                 UIImage(named: "Triangle.png")!,
+                                 UIImage(named: "X.png")!]
+    
+    let arrayTrayBase : NSArray = [UIImage(named: "base-1.png")!,
+                            UIImage(named: "base-2.png")!,
+                            UIImage(named: "base-3.png")!,
+                            UIImage(named: "base-4.png")!,
+                            UIImage(named: "base-5.png")!,
+                            UIImage(named: "base-6.png")!,
+                            UIImage(named: "base-7.png")!,
+                            UIImage(named: "base-8.png")!,
+                            UIImage(named: "base-9.png")!,
+                            UIImage(named: "base-10.png")!]
 
     // Objects of view
     @IBOutlet weak var trayBaseImg: UIImageView!
@@ -56,9 +67,12 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var pointsPlayer2Lbl: UILabel!
     @IBOutlet weak var qttGamesLbl: UILabel!
     @IBOutlet weak var qttStalemateLbl: UILabel!
+    @IBOutlet weak var refreshBtn: UIBarButtonItem!
     @IBOutlet weak var settingsBtn: UIBarButtonItem!
     @IBOutlet weak var statusPlayer1Img: UIImageView!
     @IBOutlet weak var statusPlayer2Img: UIImageView!
+    @IBOutlet weak var partPlayer1Img: UIImageView!
+    @IBOutlet weak var partPlayer2Img: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,9 +81,7 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
         let backItem = UIBarButtonItem()
         backItem.title = ""
         navigationItem.backBarButtonItem = backItem
-        
-        randomLayout()
-        
+
         // Round edge of Play again button
         //playButton.layer.cornerRadius = playButton.bounds.width / 12
         playAgainButton.layer.cornerRadius = playAgainButton.bounds.width / 12
@@ -78,9 +90,29 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
         
         game.firstGame = true
     }
+    
+    @IBAction func refreshDeleteGameButton(_ sender: Any) {
+    
+        // Create alert and define parameters title and message
+        let alert = UIAlertController(title: "Finish?", message: "You want to finish the current game?", preferredStyle: .alert)
+        
+        // Add alert to alertActions and define title OK button
+        alert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (action) -> Void in
+            
+            // Call the function to play again
+            self.playAgain()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "NO", style: .cancel, handler: {(action) -> Void in }))
+
+        // Show alert in view
+        self.present(alert, animated: true, completion: nil)
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
+        
+        randomLayout()
         
         statusPlayer1Img.image = UIImage(named: "active.png")
         statusPlayer2Img.image = UIImage(named: "disactivate.png")
@@ -94,6 +126,7 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
         if firstGame == false {
 
             settingsBtn.isEnabled = true
+            refreshBtn.isEnabled = true
             
             firstGame = false
             game.firstGame = false
@@ -132,7 +165,6 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
             
             viewFirstGame()
         }
-        randomLayout()
     }
 
     func viewFirstGame() {
@@ -217,6 +249,7 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
             self.playButton.isHidden = true
 
             self.settingsBtn.isEnabled = true
+            self.refreshBtn.isEnabled = true
         })
         
         // Set the CANCEL button
@@ -251,16 +284,18 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func playAgainPressed(_ sender: AnyObject) {
-        
-        // Call the function to random layout
-        randomLayout()
-        
+
         // Call the function to play again
         playAgain()
     }
     
     // Function to play again
     func playAgain() {
+
+        refreshBtn.isEnabled = true
+
+        // Call the function to random layout
+        randomLayout()
 
         // Define the player 1 as actived
         activePlayer = 1
@@ -296,6 +331,8 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
         // If the postion selected has a tag 0 and exist a game active
         if gameState[sender.tag] == 0 && gameActive == true {
 
+            var image = UIImage()
+            
             // More one clicks cout
             cliksCount += 1
             
@@ -304,31 +341,30 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
             // User 1 is active
             if activePlayer == 1 {
                 
+                image = imageButtonPlayer1 //UIImage(imageButtonPlayer1)!
+                
                 // Change to active the user 2
                 activePlayer = 2
                 statusPlayer2Img.image = UIImage(named: "active.png")
                 
                 // Change to disactivate to user 1
                 statusPlayer1Img.image = UIImage(named: "disactivate.png")
-                
-                randomFactImage()
             }
             // User 2 is active
             else {
                 
+                // Set the image of the position
+                //image = UIImage(named: "2 star.png")!
+                image = imageButtonPlayer2
+
                 // Change to active the user 1
                 activePlayer = 1
                 statusPlayer1Img.image = UIImage(named: "active.png")
                 
                 // Change to disactivate to user 2
                 statusPlayer2Img.image = UIImage(named: "disactivate.png")
-                
-                randomFactImage()
-                
-                // Set the image of the position
-                //image = UIImage(named: "2 star.png")!
             }
-            sender.setImage(imageButtonPlayer1, for: UIControlState())
+            sender.setImage(image, for: UIControlState())
 
             for combination in correctCombinations {
                 
@@ -338,7 +374,7 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
 
                     if gameState[combination[0]] == 2 {
                         winnerText = "\(game.namePlayer2!) winner! 😜"
-
+                        
                         // Set 0 to cliksCount
                         cliksCount = 0
 
@@ -350,12 +386,15 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
                         (UIApplication.shared.delegate as! AppDelegate).saveContext()
 
                     } else {
+                        
                         // More one point to Player 1
                         pointsPlayer1 += 1
                         game.pointsPlayer1 = Int16(pointsPlayer1)
                         pointsPlayer1Lbl.text = "\(game.pointsPlayer1)"
                     }
 
+                    self.refreshBtn.isEnabled = false
+                    
                     statusPlayer1Img.image = UIImage(named: "active.png")
                     statusPlayer2Img.image = UIImage(named: "disactivate.png")
                     
@@ -403,6 +442,8 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
             // Without winner
             if cliksCount == 9 && gameActive {
             
+                self.refreshBtn.isEnabled = false
+
                 statusPlayer1Img.image = UIImage(named: "active.png")
                 statusPlayer2Img.image = UIImage(named: "disactivate.png")
 
@@ -454,56 +495,36 @@ class HashGameVC: UIViewController, UITextFieldDelegate {
         }
     }
     
-
-    func randomFactImage() {
-
-        //random image generating method
+    // Random generating...
+    func randomLayout() {
+        
+        // Random base generating method
+        let baserange: UInt32 = UInt32(arrayTrayBase.count)
+        let randombase = Int(arc4random_uniform(baserange))
+        let generatedbase: AnyObject = arrayTrayBase.object(at: randombase) as AnyObject
+        trayBaseImg.image = (generatedbase as? UIImage)!
+        
+        // Random image 1 generating method
         let imagerange: UInt32 = UInt32(arrayOfImages.count)
         let randomimage = Int(arc4random_uniform(imagerange))
         let generatedimage: AnyObject = arrayOfImages.object(at: randomimage) as AnyObject
-        imageButtonPlayer1 = (generatedimage as? UIImage)!
-        imageButtonPlayer2 = (generatedimage as? UIImage)!
-    }
-    
-    func randomLayout() {
+
+        // Random image 2 generating method
+        let imagerange2: UInt32 = UInt32(arrayOfImages.count)
+        let randomimage2 = Int(arc4random_uniform(imagerange2))
+        let generatedimage2: AnyObject = arrayOfImages.object(at: randomimage2) as AnyObject
         
-        let randomBase = arc4random_uniform(11)
-        switch(randomBase) {
-        case 0:
-            trayBaseImg.image = UIImage(named: "base-1.png")
-            break            
-        case 1:
-            trayBaseImg.image = UIImage(named: "base-2.png")
-            break
-        case 2:
-            trayBaseImg.image = UIImage(named: "base-3.png")
-            break
-        case 3:
-            trayBaseImg.image = UIImage(named: "base-4.png")
-            break
-        case 4:
-            trayBaseImg.image = UIImage(named: "base-5.png")
-            break
-        case 5:
-            trayBaseImg.image = UIImage(named: "base-7.png")
-            break
-        case 6:
-            trayBaseImg.image = UIImage(named: "base-7.png")
-            break
-        case 8:
-            trayBaseImg.image = UIImage(named: "base-8.png")
-            break
-        case 8:
-            trayBaseImg.image = UIImage(named: "base-9.png")
-            break
-        case 9:
-            trayBaseImg.image = UIImage(named: "base-10.png")
-            break
-        default:
-            trayBaseImg.image = UIImage(named: "base-10.png")
-            break;
+        // If images is equals, then random image again
+        if (generatedimage as? UIImage)! == (generatedimage2 as? UIImage)! {
+            randomLayout()
+        }
+        // If images is not equals
+        else {
+            imageButtonPlayer1 = (generatedimage as? UIImage)!
+            imageButtonPlayer2 = (generatedimage2 as? UIImage)!
+            partPlayer1Img.image = (generatedimage as? UIImage)!
+            partPlayer2Img.image = (generatedimage2 as? UIImage)!
         }
     }
-
 }
 
